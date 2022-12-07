b0e2f5fc17117f54b50ec27ae863580b86b6b9e0
chirag solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-12-07 18:37:48 +0530

    Delete Day7 directory

--- Day7/power_calculator.py
+++
@@ -1,4 +0,0 @@
-base=(int)(input("Enter base of Number :"));
-power=(int)(input("Enter power of Number :"));
-
-print(base,"raise to",power,"is =",base**power); #here ** is a Exponentiation Operator 
