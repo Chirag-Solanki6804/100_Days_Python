31eaca023f0d2098c33ab9a76dd74a336e1da390
chirag solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-12-07 18:39:51 +0530

    Add files via upload

---
+++ Day05/main.py
@@ -0,0 +1,27 @@
+# Comments, Escape Sequences & Print Statement | Python Tutorial - Day #5
+
+# Single line Comments
+print("Hello Chirag");
+
+#multiLine Commments
+
+"""
+hello this is
+a multi line
+comment
+"""
+print("above sentense is a multiLine Comment");
+
+
+# Escape Sequences 
+print("Hello , everone my name is chirag solanki\nI am studying in \" darshan university \" ");
+
+# here, \n  is a Escape Sequences to break Line
+#and \" is a also a Escape Sequences
+
+
+print("Chirag",7,23,"solanki");
+print("Chirag",7,23,"solanki",sep="~",end="\njay hind\n");
+
+
+
