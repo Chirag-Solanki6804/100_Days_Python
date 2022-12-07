0891477e396ca9bf17a191a6750327476bea3f91
chirag solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-12-07 18:35:35 +0530

    Add files via upload

---
+++ Day10/userInput.py
@@ -0,0 +1,18 @@
+# Taking User Input in Python | Python Tutorial - Day #10
+
+# variable=input();
+# print(variable);
+
+# name=input("Enter Your Name :");
+# print("Your Name is",name);
+
+num1=int(input("Enter First Number :"));
+num2=int(input("Enter Second Number :"));
+
+print("Addition of",num1,"and",num2,"is :",num1+num2);
+
+print("subtraction of",num1,"and",num2,"is :",num1-num2);
+
+print("multiplication of",num1,"and",num2,"is :",num1*num2);
+
+print("division of",num1,"and",num2,"is :",num1/num2);
